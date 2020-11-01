6337bea24eeb12e445150468ea7352bdc31086e4
Anthony <ahanel13@gmail.com>
2020-10-31 22:42:17 -0500

    migrated reference from passwords to user

--- db/schema.rb
+++ db/schema.rb
@@ -17,8 +17,10 @@ ActiveRecord::Schema.define(version: 2020_11_01_031723) do
     t.string "name"
     t.string "notes"
     t.string "password"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_secure_passwords_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
