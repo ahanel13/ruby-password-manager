a3454d3158d717d811bd5c07066342c8aa1c7f48
Anthony <ahanel13@gmail.com>
2020-11-02 10:24:03 -0600

    finished UI for sign in page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   get 'home/login'
   get 'home/signup'
-=======
   resources :secure_passwords
   devise_for :users
   root to: "home#index"
 
   get '/dashboard/index'
->>>>>>> master
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
