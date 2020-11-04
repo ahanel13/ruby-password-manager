e82919b65b6d2f633418e2ac2d7cefd134bd4059
Anthony <ahanel13@gmail.com>
2020-11-04 13:56:19 -0600

    transfered rails app to use postgresql locally, now will set up for deployment to heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,15 @@
 
 ActiveRecord::Schema.define(version: 2020_11_01_031723) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "secure_passwords", force: :cascade do |t|
     t.string "url"
     t.string "name"
     t.string "notes"
     t.string "password"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_secure_passwords_on_user_id"
